800e0f0f34a561fbcd482f6200008694feb48425
Jeremy Valentine <38669521+valentine195@users.noreply.github.com>
2024-03-29 11:06:01 -0400

    chore: update types

--- src/events/event.types.ts
+++ src/events/event.types.ts
@@ -1,47 +1,10 @@
-/* export enum EventType {
-    Recurring,
-    Range,
-    Date,
-} */
-export const EventType = Object.freeze({
+export const EventType = {
     Date: "Date",
     Range: "Range",
     Recurring: "Recurring",
     Undated: "Undated",
     // …
-});
+} as const;
 
 // Overload the type
 export type EventType = (typeof EventType)[keyof typeof EventType];
-
-export enum EventCombinator {
-    AND,
-    OR,
-    NAND,
-    XOR,
-}
-export enum EventOperator {
-    Plus,
-    Minus,
-}
-export enum EventCondition {
-    Equals,
-    GreaterThan,
-    GreaterThanOrEqual,
-    LessThan,
-    LessThanOrEqual,
-    Every,
-}
-export enum EventTimespan {
-    Date,
-    Year,
-    Month,
-    MonthNumber,
-    Week,
-    WeekNumber,
-    WeekNumberInMonth,
-
-    Moon,
-    Random,
-    Event,
-}
