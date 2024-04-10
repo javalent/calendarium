bc0df407d418c8e2fb6fa17825a2af8ab0508e7c
Jeremy Valentine <38669521+valentine195@users.noreply.github.com>
2024-04-10 08:41:31 -0400

    chore: extract types to 1 file

---
+++ src/@types/obsidian-ext.d.ts
@@ -0,0 +1,71 @@
+import "obsidian";
+
+declare module "obsidian" {
+    interface App {
+        internalPlugins: {
+            getPluginById(id: "daily-notes"): {
+                _loaded: boolean;
+                instance: {
+                    options: {
+                        format: string;
+                    };
+                };
+            };
+            getPluginById(id: "sync"): {
+                _loaded: boolean;
+                instance: Events & {
+                    getStatus():
+                        | "error"
+                        | "paused"
+                        | "syncing"
+                        | "uninitialized"
+                        | "synced";
+                    on(name: "status-change", callback: () => any): EventRef;
+                };
+            };
+            getPluginById(id: "page-preview"): {
+                _loaded: boolean;
+            };
+        };
+        setting: {
+            open(): void;
+            close(): void;
+            openTabById(id: string): CalendariumSettings;
+        };
+
+        plugins: {
+            plugins: {
+                "fantasy-calendar": Plugin;
+            };
+        };
+    }
+    interface Workspace {
+        trigger(name: "parse-style-settings"): void; // trigger style settings
+        on(name: "calendarium-updated", callback: () => any): EventRef;
+        on(
+            name: "calendarium-event-update",
+            callback: (tree: CalendarEventTree) => any
+        ): EventRef;
+        on(name: "calendarium-settings-change", callback: () => any): EventRef;
+        trigger(name: "calendarium-updated"): void;
+        trigger(name: "calendarium-settings-change"): void;
+        trigger(name: "calendarium-settings-external-load"): void;
+        trigger(
+            name: "calendarium-event-update",
+            tree: CalendarEventTree
+        ): void;
+        trigger(
+            name: "link-hover",
+            popover: any, //hover popover, but don't need
+            target: HTMLElement, //targetEl
+            note: string, //linkText
+            source: string //source
+        ): void;
+        trigger(name: "calendarium-settings-loaded"): void;
+        on(name: "calendarium-settings-loaded", callback: () => any): EventRef;
+        on(
+            name: "calendarium-settings-external-load",
+            callback: () => any
+        ): EventRef;
+    }
+}
